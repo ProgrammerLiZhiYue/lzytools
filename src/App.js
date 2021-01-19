f17b98ea5d0889b08bc62c34672e9fa66853989c
lizhiyue <zhiyue.li@ly.com>
2021-01-19 15:02:07 +0800

    转换淘宝链接

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "antd-mobile/dist/antd-mobile.css";
+import { WingBlank, TextareaItem, Button,Card } from "antd-mobile";
+import { useState } from "react";
+import { parse } from "url";
+
 
 function App() {
+  const [originString, setOriginString] = useState("");
+  const [convertedString, setConvertedString] = useState("");
+
+  function convert() {
+    const id = parse(originString, true)
+      .query.id;
+    setConvertedString(`https://h5.m.taobao.com/cart/order.html?buyParam=${id}_1`);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WingBlank>
+        <TextareaItem
+          autoHeight
+          value={originString}
+          onChange={(e) => setOriginString(e)}
+        ></TextareaItem>
+      </WingBlank>
+      <WingBlank>
+        <Button type="primary" onClick={convert}>
+          转换
+        </Button>
+      </WingBlank>
+      <WingBlank>
+        <Card>
+          <Card.Header>转换结果</Card.Header>
+          <Card.Body>
+            <div><span>{convertedString}</span></div>
+          </Card.Body>
+        </Card>
+      </WingBlank>
     </div>
   );
 }
